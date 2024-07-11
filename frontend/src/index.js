fb77674689a805891d31dc36d46ce94edc464043
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-11 22:19:50 +0530

    edit the menu options pt3
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Landing from "./components/Muse/Create/Landing";
-import Explore from "./components/Muse/Explore/Explore"; // Create this component
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to="/create-muse" replace />} />
+      <Route path="/" element={<Navigate to="/create-muse" />} />
       <Route path="/create-muse" element={<Landing />} />
-      <Route path="/explore" element={<Explore />} />
+      <Route path="/explore" element={<div>Explore Page</div>} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
