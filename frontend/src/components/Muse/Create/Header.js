f689d18d7f900e201110325b1f5ae219a9feedf2
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-11 20:27:59 +0530

    format the pages and add menu options
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- frontend/src/components/Muse/Create/Header.js
+++ frontend/src/components/Muse/Create/Header.js
@@ -1,16 +1,30 @@
-import '../../../Create-muse.css';
+import React from "react";
+import { Link } from "react-router-dom"; // Assuming you use React Router for navigation
+import "../../../Create-muse.css";
 
 const Header = () => {
-    return (
-      <header className="header">
-        <div className="header-left">
-          <img src="https://placehold.co/40x40" alt="logo" className="logo" />
-        </div>
-        <div className="header-right">
-          <img src="https://placehold.co/40x40" alt="user avatar" className="avatar" />
-        </div>
-      </header>
-    );
-  };
+  return (
+    <header className="header">
+      <div className="header-left">
+        <img src="https://placehold.co/40x40" alt="logo" className="logo" />
+        <nav>
+          <Link to="/explore" className="header-link">
+            Explore
+          </Link>
+          <Link to="/" className="header-link active">
+            Create Muse
+          </Link>
+        </nav>
+      </div>
+      <div className="header-right">
+        <img
+          src="https://placehold.co/40x40"
+          alt="user avatar"
+          className="avatar"
+        />
+      </div>
+    </header>
+  );
+};
 
-  export default Header;
+export default Header;
--- frontend/src/components/Muse/Create/Landing.js
+++ frontend/src/components/Muse/Create/Landing.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import Header from '../Create/Header'
-import MainSection from '../Create/MainSection'
-import '../../../Create-muse.css';
-
+import React from "react";
+import Header from "../Create/Header";
+import MainSection from "../Create/MainSection";
+import "../../../Create-muse.css";
 
 const Landing = () => {
   return (
     <div>
-    <Header />
-    <main className="main-content">
-    <MainSection />
-    </main>
+      <Header />
+      <main className="main-content">
+        <MainSection />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
