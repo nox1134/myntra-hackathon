d7a956423581810795568ba96290d98c049a241b
raaksss <rakshitamanocha@gmail.com>
2024-07-12 01:10:00 +0530

    Add header to explore muse page
    Signed-off-by: raaksss <rakshitamanocha@gmail.com>

--- frontend/src/components/Muse/Explore/Explore.js
+++ frontend/src/components/Muse/Explore/Explore.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Create/Header';
 
 const Explore = () => {
   return (
     <div>
-      <h1>Explore Page</h1>
+      <Header />
     </div>
   );
 };
