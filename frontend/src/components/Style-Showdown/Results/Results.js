caf627180b4c94f2df89f60c7306534587a7cd66
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-12 23:02:20 +0530

    add confetti to results page
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- frontend/src/components/Style-Showdown/Results/Results.js
+++ frontend/src/components/Style-Showdown/Results/Results.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 import Header from '../Results/Header';
 import HeroSection from '../Results/HeroSection';
 import StyleIcon from '../Results/StyleIcon';
@@ -7,8 +8,17 @@ import ProductsSection from '../Results/ProductsSection';
 import './Results.css';
 
 function Results() {
+  const [showConfetti, setShowConfetti] = useState(true);
+
+  useEffect(() => {
+    // Hide confetti after 5 seconds
+    const timer = setTimeout(() => setShowConfetti(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="relative flex size-full min-h-screen flex-col bg-[#fcf8fa] justify-between group/design-root overflow-x-hidden">
+    <div className="relative flex size-full min-h-screen flex-col bg-[#fcf8fa] justify-between group/design-root overflow-x-hidden" style={{ fontFamily: '"Be Vietnam Pro", "Noto Sans", sans-serif' }}>
+      {showConfetti && <Confetti />}
       <div>
         <Header />
         <HeroSection />
