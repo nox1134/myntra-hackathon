bd601e7b195852193630b13df293cff129de7626
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-14 02:22:21 +0530

    add interactive particles bg to quiz landing
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- frontend/src/components/Style-Showdown/Landing/QuizLanding.js
+++ frontend/src/components/Style-Showdown/Landing/QuizLanding.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Header from './Header'; 
-import Main from './Main'; 
+import Header from './Header';
+import Main from './Main';
+import Particles from './Particles'; // Adjust the path if necessary
 import '../../../index.css';
-import './landing.css'
+import './landing.css';
 
 const QuizLanding = () => {
   return (
     <div className="flex flex-col items-center justify-center p-4 relative z-10">
+      <Particles className="absolute inset-0 z-0" color="#333333" />
       <Header />
       <Main />
     </div>
