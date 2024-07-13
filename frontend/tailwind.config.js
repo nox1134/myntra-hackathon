607790aff25240b05cd843a0387b8356105c3373
Priyanshi Gaur <noxdot1134@gmail.com>
2024-07-13 18:52:14 +0530

    update config files
    Signed-off-by: Priyanshi Gaur <noxdot1134@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
+// tailwind.config.js
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        backgroundPositionSpin: 'background-position-spin 3000ms infinite alternate',
+      },
+      keyframes: {
+        'background-position-spin': {
+          '0%': { backgroundPosition: 'top center' },
+          '100%': { backgroundPosition: 'bottom center' },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
